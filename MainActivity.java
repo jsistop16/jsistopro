b9b2947af6fd7a490b3b5810fdb22c3362cef790
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-07-26 23:45:47 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,112 @@
-package com.example.mytoast;
+package com.example.myevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Gravity;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.view.Gravity.*;
-
 public class MainActivity extends AppCompatActivity {
+
+    View view;
+    TextView textView;
+    GestureDetector detector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView) findViewById(R.id.textView);
+
+        view = findViewById(R.id.View);
+        view.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트", Toast.LENGTH_LONG);
-                toast.setGravity(TOP|LEFT,200, 200);
-                toast.show();
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+
+                float curX = event.getX();
+                float curY = event.getY();
+
+                if (action == MotionEvent.ACTION_DOWN){
+                    println("손가락 눌림 : " + curX + "," + curY);
+                } else if (action == MotionEvent.ACTION_MOVE){
+                    println("손가락 움직임 : " + curX + "," + curY);
+                } else if (action == MotionEvent.ACTION_UP){
+                    println("손가락 뗌 : " + curX + "," + curY);
+                }
+
+                return true;
+
             }
         });
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onDown(MotionEvent e) {
+                println("onDown() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+                println("onShowPress() 호출됨.");
 
             }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("onSingleTapUp() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                println("onScroll() 호출됨." + distanceX + ',' + distanceY);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+                println("onLongPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                println("onFling() 호출됨." + velocityX + ',' + velocityY);
+                return true;
+            }
         });
+
+        View view2 = findViewById(R.id.View2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            Toast.makeText(this, "시스템 BACK 버튼 눌림.", Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+        return false;
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
     }
 }
